474b621a5e7c05f879098a89fda92d3b412f8d11
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2025-05-26 00:02:44 +0200

    tw

--- editor.js
+++ editor.js
@@ -214,7 +214,7 @@ const modalHTML = `
                                             class="editor-icon-button" title="Move Down" :disabled="index === formData.length - 1">
                                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="5" x2="12" y2="19"/><polyline points="19 12 12 19 5 12"/></svg>
                                         </button>
-                                        <button @click.stop="formData.splice(index, 1)" class="editor-icon-button" title="Remove Item" :disabled="formData.length <= 1">
+                                        <button @click.stop="if (window.confirm('Are you sure you want to delete this item?')) { formData.splice(index, 1); }" class="editor-icon-button" title="Remove Item" :disabled="formData.length <= 1">
                                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="5" y1="12" x2="19" y2="12"/></svg>
                                         </button>
                                     </div>
